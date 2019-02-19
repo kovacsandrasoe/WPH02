ae5e8739c22533ae995f050056405ae0e59da92c
Kovács András <kovi91@gmail.com>
2019-02-19 17:44:50 +0100

    Add project files.

---
+++ 01_LINQ_XML/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_LINQ_XML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
